d0236c7b062d607613d8f330582ba2691b6f3397
vinilavarda <vinicius.lavarda@yahoo.com.br>
2022-04-01 17:34:39 -0300

    final

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -42,6 +42,62 @@ class Atendimento {
     }
 
   }
+
+  lista(res) {
+    const sql = 'SELECT * FROM Atendimentos'
+
+    conexao.query(sql, (erro, resultados) => {
+      if (erro) {
+        res.status(400).json(erro)
+      } else {
+        res.status(200).json(resultados)
+      }
+    })
+  }
+
+  buscaPorId(id, res) {
+    const sql = `SELECT * FROM Atendimentos WHERE id = ${id}`
+
+    conexao.query(sql, (erro, resultados) => {
+      const atendimentoUnico = resultados[0]
+      if (erro) {
+        res.status(400).json(erro)
+      } else {
+        res.status(200).json(atendimentoUnico)
+      }
+    })
+  }
+
+  altera(id, valores, res) {
+
+    if (valores.data) {
+      valores.data = moment(valores.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+    }
+
+    const sql = 'UPDATE Atendimentos SET ? WHERE id=?'
+
+    conexao.query(sql, [valores, id], (erro, resultados) => {
+      if (erro) {
+        res.status(400).json(erro)
+      } else {
+        res.status(201).json({ ...valores, id })
+      }
+    })
+
+  }
+
+  deleta(id, res) {
+    const sql = 'DELETE FROM Atendimentos WHERE id=?'
+
+    conexao.query(sql, id, (erro, resultados) => {
+      if (erro) {
+        res.status(400).json(erro)
+      } else {
+        res.status(201).json({ id })
+      }
+    })
+  }
+
 }
 
 module.exports = new Atendimento
